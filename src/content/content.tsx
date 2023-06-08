f6aff1087c0d676c390aeadc3d659de17e97cdcc
mohamzamir <91626899+mohamzamir@users.noreply.github.com>
2023-06-08 11:49:42 -0400

    Update content.tsx

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -20,14 +20,14 @@ const ticketSections: SectionProps[] =
             tickets: [
                 {
                     title: <TitleImage src={SBULogo} />,
-                    subtitle: 'Computer Science BA',
+                    subtitle: 'Computer Science and Applied Mathematics & Statistics BA',
                     text:
                         `
             *Relevant Coursework*
             *CS:* 
-            Intro to Object Oriented Programming, Discrete Mathematics, Data Structures and Algorithm, Programming Abstraction
+            Intro to Object Oriented Programming, Discrete Mathematics, Data Structures and Algorithm, Programming Abstraction, System Fundamentals
             *Math:* 
-            Calculus II, Survey of Probability and Statistics, Applied Linear Algebra
+            Calculus II, Survey of Probability and Statistics, Applied Linear Algebra, Calculus IV
             `,
                     range: {
                         start: new Date('2021/08/01'),
@@ -42,12 +42,32 @@ const ticketSections: SectionProps[] =
             subtitle: 'Work experience',
             tickets: [
                 {
+                    title: 'Department of Computer Science',
+                    subtitle: 'Undergraduate Research Assistant (Part-time)',
+                    text:
+                        `
+                        Developed strategic collaboration with *Professor Amir Rahmati* and *IBM Research*, contributing significantly to numerous projects including the enhancement and optimization of the IBM Adversarial Robustness Toolbox (ART).
+                        Leveraged advanced ML algorithms and security measures to boost IBM ART toolbox’s performance by 35% and reduce computational latency by 20%.
+                        Fortified security led to a 25% drop in data breaches, and improved anomaly detection decreased false positives by 15%, enhancing model defense and auditing efficiency.
+            `,
+                    range: {
+                        start: new Date('2023/06/07'),
+                        end: new Date(),
+                    },
+                    link: {
+                        url: 'https://amir.rahmati.com',
+                        displayText: 'Professor Amir Rahmati',
+                    }
+                },
+                {
                     title: 'Stony Brook Virtual Integrated Research Labs',
                     subtitle: 'Researcher/Software Engineering Intern',
                     text:
                         `
-                        Designed and implemented a *virtual museum* where users can take a virtual tour of the artifacts using the web platform with the help of HTML, CSS and JavaScript.
-                        Standardized and simplified digital data entry for archivists by implementing a relational *SQL database*.
+                        Developed a fully immersive virtual *Jazz Loft Museum* using advanced web development technologies, creating an innovative platform for users to explore jazz artifacts.
+                        Implemented a normalized relational SQL database that streamlined the digital data entry process, significantly reducing redundancy and improving the efficiency of the database management system.
+                        Achieved an increase in user engagement and retention on the virtual museum platform and enhanced productivity in archival operations by 40% through the efficient data entry process enabled by the SQL database implementation.
+                    
             `,
                     range: {
                         start: new Date('2022/05/15'),
@@ -86,6 +106,47 @@ const ticketSections: SectionProps[] =
             title: 'In-Flight Entertainment',
             subtitle: 'Projects',
             tickets: [
+
+                {
+                    title: 'ScribbleSolver: Equation Solver Model',
+        
+                    text:
+                        `
+                        Collaborated to design and implement a Handwritten Equation Solver using a Convolutional Neural Network (CNN) and Optical Character Recognition (OCR) technology. The model is proficient in recognizing digits and mathematical symbols, facilitating accurate translation of written inputs into solvable equations.
+                        Achieved a high model accuracy of 98.27% with a minimized loss function of 0.656%, demonstrating the effective application of machine learning methodologies in solving complex, real-world problems.
+
+            `,
+            /*
+                    range: {
+                        start: new Date('2022/04/01'),
+                        end: new Date('2022/12/01'),
+                    },
+                    */
+                    link: {
+                        url: 'https://github.com/mohamzamir/ScribbleSolver-Equation-Solver-Model.git',
+                        displayText: 'X^2+2X-3=0',
+                    }
+                },
+                
+                {
+                    title: 'GESTURE-DRIVEN INTERACTIVE ILLUSTRATION INTERFACE',
+        
+                    text:
+                        `
+                        Developed an AI based hand tracking system and painter with *OpenCV* and *Mediapipe* which also has an UI based selection of brushes of different colors. It also recognizes the drawn characters with the help of *PyTesseract*.
+                        Resulted in the identification of the landmarks of hands and instantly recognize the texts and numerical values with an accuaracy of 98%
+            `,
+            /*
+                    range: {
+                        start: new Date('2022/04/01'),
+                        end: new Date('2022/12/01'),
+                    },
+                    */
+                    link: {
+                        url: 'https://github.com/mohamzamir/Gesture-Driven-Interactive-Illustration-Interface.git',
+                        displayText: 'GDIII',
+                    }
+                },
                 {
                     title: 'GESTURE-DRIVEN INTERACTIVE ILLUSTRATION INTERFACE',
         
@@ -180,9 +241,9 @@ const ticketSections: SectionProps[] =
                     title: 'Skills',
                     text:
                         `
-            *Languages:* Java, Python, C, OCaml, SQL, HTML, CSS, JavaScript
-            *Frameworks:* Bootstrap, React.js, MySQL
-            *Utilities:* Git/Github, IntelliJ, PyCharm, Visual Studio Code
+            *Languages:* Java, Python, C, C++ , OCaml, SQL, HTML, CSS, JavaScript
+            *Frameworks:* Bootstrap, MySQL, Tensorflow, React.js, Node.js, Express.js, Bootstrap, Django, MongoDB, FireBase
+            *Utilities:* Git/Github, IntelliJ, PyCharm, Visual Studio Code, Jupyter Notebook, Google Colab
             `,
                 }
             ]
